e6fa6974b545e5c704053a7550c18ca2648dcf0d
Youssef-ADOUIRI <youssef.galaoui@gmail.com>
2023-03-02 21:57:28 +0100

    fixing the dev env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,10 +55,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
-CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST = (
-#     'https://esg-olive.vercel.app',
-# )
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'https://esg-olive.vercel.app',
+    'http://localhost:3000'
+)
 
 ROOT_URLCONF = 'backend.urls'
 
--- frontend/src/axios_api.js
+++ frontend/src/axios_api.js
@@ -2,4 +2,5 @@ import axios from "axios";
 
 export const MyApiClient = axios.create({
   baseURL: "https://esg-live.vercel.app/",
+  timeout: 10000,
 });
