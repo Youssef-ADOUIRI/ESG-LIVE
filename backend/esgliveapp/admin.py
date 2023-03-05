074583ddb6fcb75df5b79c786a153dd779d347c2
Youssef-ADOUIRI <youssef.galaoui@gmail.com>
2023-03-05 18:14:39 +0100

    update admin site models

--- backend/esgliveapp/admin.py
+++ backend/esgliveapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from esgliveapp.models import Team, Player , AthleticsMatch , AthleticsParticipation,CollectiveMatch, DetailsMatch  
+from esgliveapp.models import Team, Player , AthleticsMatch , AthleticsParticipation , CollectiveMatch , DetailsMatch  , TeamRanking
 
-admin.site.register([Team, Player , AthleticsMatch , AthleticsParticipation,CollectiveMatch, DetailsMatch  ])
+admin.site.register([Team, Player , AthleticsMatch , AthleticsParticipation,CollectiveMatch, DetailsMatch , TeamRanking ])
