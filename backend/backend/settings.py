b62df422f8c842bd8a78eaf3773c8981630a1948
Youssef-ADOUIRI <youssef.galaoui@gmail.com>
2023-03-11 13:53:40 +0100

    deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a-oq$pol&v7%@f&u8$#7@!^%88fk9_&#6cw$nw^mjs6w5&%d8n'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
